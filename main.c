060672fea43d604ffc59c6d66fe6f532c3ab58fb
eunso0713 <aliennumber1one@sookmyung.ac.kr>
2023-11-11 01:46:11 +0900

    06

--- main.c
+++ main.c
@@ -1,32 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-int main(void) {
-
-    FILE *fp = NULL;
-    fp = fopen("sample.txt", "a");
-    if (fp == NULL) {
-        printf("File not opened\n");
-        return 1;
-    }
-    char a, b, c;
-    
-    printf("Input a word: ");
-    scanf(" %c", &a);
-    
-    while (getchar() != '\n');
-    
-    printf("Input a word: ");
-    scanf(" %c", &b);
-    
-    while (getchar() != '\n');
-    
-    printf("Input a word: ");
-    scanf(" %c", &c);
-    
-    fprintf(fp, "%c%c%c", a, b, c);
-    
-    fclose(fp);
-    
-    return 0;
+void main(void) {
+	FILE *fp = NULL;
+	char c;
+	fp = fopen("sample.txt", "r");
+	if (fp == NULL)
+		printf("file not opened\n");
+	
+	while((c=fgetc(fp)) != EOF)
+		putchar(c);
+	
+	fclose(fp);
 }
