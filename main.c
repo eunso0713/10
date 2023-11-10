778ca01ee62ae89920d0424411da5216ae4ad721
eunso0713 <aliennumber1one@sookmyung.ac.kr>
2023-11-11 01:05:05 +0900

    03

--- main.c
+++ main.c
@@ -1,20 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
-
-int main(void) {
-	int i = 0;
-	char str[4];
-	str[0] = 'a';
-	str[1] = 'b';
-	str[2] = 'c';
-	str[3] = '\0';
+#include <string.h>
+void main(void) {
+	char src[] = "The worst things to eat before you sleep";
+	char dst[100];
 	
-	printf("%s\n", str);
+	strcpy(dst, src);
 	
-	while (str[i] != '\0'){
-		printf("%c\n", str[i]);
-		i++;
-	}
-	
-	return 0;
+	printf("copied string : %s", dst);
 }
