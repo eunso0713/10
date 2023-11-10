6345915540a9b74de11a34ce37a8ce140f1d4a8e
eunso0713 <aliennumber1one@sookmyung.ac.kr>
2023-11-11 01:08:00 +0900

    04

--- main.c
+++ main.c
@@ -2,10 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 void main(void) {
-	char src[] = "The worst things to eat before you sleep";
-	char dst[100];
+	char str[30] = "happy C programming";
 	
-	strcpy(dst, src);
-	
-	printf("copied string : %s", dst);
+	printf("문자열 \"%s\"의 길이 : %i", str, strlen(str));
 }
